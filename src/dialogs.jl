de52062aedee7f3b2faf63d5eb7f6b7d8f38cbae
Eric Shain <eric.shain@gmail.com>
2015-02-28 18:16:07 -0600

    Added keywords to file dialog functions
    Edited GetOpenFIle(), GetSaveFile() and ChooseDirectory() to add Tk
    keywords

--- src/dialogs.jl
+++ src/dialogs.jl
@@ -1,9 +1,32 @@
 ## dialogs
 
-## can add arguments if desired. Don't like names or lack of arguments
-GetOpenFile() = tcl("tk_getOpenFile")
-GetSaveFile() = tcl("tk_getSaveFile")
-ChooseDirectory() = tcl("tk_chooseDirectory")
+## File dialogs functions. Tk keywords added
+function GetOpenFile(;filetypes::Vector=String[], initialdir::String="",
+                     initialfile::String="", multiple::Bool=false, title::String="")
+  args = Dict()
+  if length(filetypes) > 0 args["filetypes"] = filetypes end
+  if length(initialdir) > 0 args["initialdir"] = initialdir end
+  if length(initialfile) > 0 args["initialfile"] = initialfile end
+  args["multiple"] = multiple
+  if length(title) > 0 args["title"] = title end
+  tcl("tk_getOpenFile", args)
+end
+function GetSaveFile(;filetypes::Vector=String[], initialdir::String="",
+                    initialfile::String="", title::String="")
+  args = Dict()
+  if length(filetypes) > 0 args["filetypes"] = filetypes end
+  if length(initialdir) > 0 args["initialdir"] = initialdir end
+  if length(initialfile) > 0 args["initialfile"] = initialfile end
+  if length(title) > 0 args["title"] = title end
+  tcl("tk_getSaveFile", args)
+end
+function ChooseDirectory(;initialdir::String="",mustexist=false,title::String="")
+  args = Dict()
+  if length(initialdir) > 0 args["initialdir"] = initialdir end
+  if length(title) > 0 args["title"] = title end
+  args["mustexist"] = mustexist
+    tcl("tk_chooseDirectory", args)
+end
 
 ## Message box
 function Messagebox(parent::MaybeWidget; title::String="", message::String="", detail::String="")
